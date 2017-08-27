c7fb676ca215ad486f69dc135e1af601c56c44cd
uFrost <jcub142@gmail.com>
2017-08-27 11:53:29 -0700

    [+] Main Menu System

---
+++ SDGJ2017/Assets/Scripts/Core/ButtonSwap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSwap : MonoBehaviour {
+
+    Sprite init, hover;
+    public string hoverSpriteName;
+	void Start () {
+        init = GetComponent<Image>().sprite;
+        hover = Resources.Load<Sprite>("buttonsprites/" + hoverSpriteName);
+	}
+
+    void OnMouseEnter()
+    {
+        GetComponent<Image>().sprite = hover;
+    }
+    void OnMouseExit()
+    {
+        GetComponent<Image>().sprite = init;
+    }
+    
+    
+}
---
+++ SDGJ2017/Assets/Scripts/Core/MenuManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour {
+
+    List<Text[]> scores = new List<Text[]>();
+
+    void Start () {
+        
+        for (int i = 0; i<10; i++)
+        {
+            scores.Add(new Text[2] {
+                GameObject.Find("slot" + i).GetComponent<Text>(),
+                GameObject.Find("score" + i).GetComponent<Text>()
+            });
+        }
+    }
+	
+    public void setScore(int position, string name, string score)
+    {
+        scores[position][0].text = name;
+        scores[position][1].text = score;
+    }
+    
+
+    public void LoadLevel(string s)
+    {
+        SceneManager.LoadScene(s);
+    }
+}
